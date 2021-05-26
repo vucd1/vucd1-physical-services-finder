d83e58239a77447377725f96c722410ca49ee3bb
18cpham3 <phamcathy6575@gmail.com>
2021-05-26 13:30:24 -0700

    Added routing to sign up page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,10 +15,10 @@ const routes: Routes = [
     path: 'login',
     loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
   },
-  // {
-  //   path: 'signup',
-  //   loadChildren: () => import('./signup/signup.module').then( m => m.SignupPageModule)
-  // },
+  {
+    path: 'signup',
+    loadChildren: () => import('./signup/signup.module').then( m => m.SignupPageModule)
+  },
   {
     path: 'home',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule),
@@ -28,18 +28,18 @@ const routes: Routes = [
   //   redirectTo: 'home',
   //   pathMatch: 'full'
   // },
-  // {
-  //   path: 'bookmarks',
-  //   loadChildren: () => import('./bookmarks/bookmarks.module').then( m => m.BookmarksPageModule)
-  // },
+  {
+    path: 'bookmarks',
+    loadChildren: () => import('./bookmarks/bookmarks.module').then( m => m.BookmarksPageModule)
+  },
   // {
   //   path: 'profile',
   //   loadChildren: () => import('./profile/profile.module').then( m => m.ProfilePageModule)
   // },
-  // {
-  //   path: 'search',
-  //   loadChildren: () => import('./search/search.module').then( m => m.SearchPageModule)
-  // },
+  {
+    path: 'search',
+    loadChildren: () => import('./search/search.module').then( m => m.SearchPageModule)
+  },
 ];
 
 @NgModule({
