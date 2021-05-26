6bfceb1d7361663f3610a62bb2d1d19436968385
18cpham3 <phamcathy6575@gmail.com>
2021-05-26 01:38:38 -0700

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,25 +3,30 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule),
+    path: 'WelcomePage',
+    loadChildren: () => import('./welcome/welcome.module').then( m => m.WelcomePageModule)
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'WelcomePage',
     pathMatch: 'full'
   },
-  // {
-  //   path: 'welcome',
-  //   loadChildren: () => import('./welcome/welcome.module').then( m => m.WelcomePageModule)
-  // },
-  // {
-  //   path: 'login',
-  //   loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
-  // },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
   // {
   //   path: 'signup',
   //   loadChildren: () => import('./signup/signup.module').then( m => m.SignupPageModule)
+  // },
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule),
+  },
+  // {
+  //   path: '',
+  //   redirectTo: 'home',
+  //   pathMatch: 'full'
   // },
   // {
   //   path: 'bookmarks',
