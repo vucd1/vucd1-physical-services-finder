14d03f55655029b17f503ed5c802f6f09d877011
18cpham3 <phamcathy6575@gmail.com>
2021-05-27 02:47:59 -0700

    Updated home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-// import { WelcomePage } from './welcome/welcome.page';
-// import { LoginPage } from './login/login.page';
-// import { SignupPage } from './signup/signup.page';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
+import { NavbarComponent } from '../navbar/navbar.component';
 
 import { HomePageRoutingModule } from './home-routing.module';
 
@@ -14,6 +15,7 @@ import { HomePageRoutingModule } from './home-routing.module';
     IonicModule,
     HomePageRoutingModule
   ],
-  declarations: [HomePage]
+  declarations: [HomePage,
+    NavbarComponent]
 })
 export class HomePageModule {}
