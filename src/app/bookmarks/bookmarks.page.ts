a3cfe18ef3790a8da8cb2a35b17827c14883d237
18cpham3 <phamcathy6575@gmail.com>
2021-05-28 22:05:03 -0700

    Updated bookmarks page

--- src/app/bookmarks/bookmarks.page.ts
+++ src/app/bookmarks/bookmarks.page.ts
@@ -6,19 +6,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bookmarks.page.scss'],
 })
 export class BookmarksPage implements OnInit {
-  bookmarked = [{img: '/assets/imgs/fillingStation.png', name: 'Water Filling Station ', address: 'Rowland Hall, Near First Floor Elevator', bookmark: true}, 
+  bookmarked:any = [{img: '/assets/imgs/fillingStation.png', name: 'Water Filling Station ', address: 'Rowland Hall, Near First Floor Elevator', bookmark: true}, 
   {img: '/assets/imgs/restroom.png', name: 'Restroom ', address: 'Anteater Recreation Center	Room #104', bookmark: true}, 
   {img: '/assets/imgs/langsonLib.png', name: 'Langson Library', address: '23 W Peltason Dr, Irvine CA 92670', bookmark: true},
   {img: '/assets/imgs/PhoenixFoodCourt.png', name: 'Phoenix Food Court', address: 'Ring Rd, Irvine, CA 92697', bookmark: true},
   {img: '/assets/imgs/bikeParking.png', name: 'Bike Parking', address: 'Engineering Tower, Irvine, CA 92697',  bookmark: true}]
-  constructor() { }
+  allFalse:boolean = true;
+  bookmarkedcopy = this.bookmarked.slice()
+
+  constructor() { 
+    
+  }
+  
 
   ngOnInit() {
+    
   }
 
   onClick(event: Event){
     let e = event.target as HTMLButtonElement;
     this.bookmarked[e.id].bookmark = false;
+    console.log(e.id)
+    
+    this.bookmarkedcopy.pop((this.bookmarked)[e.id]);
+    console.log(this.bookmarkedcopy);
+    // this.bookmarked.forEach(bookmark => {
+    //   if(bookmark.bookmark == true){
+    //     this.allFalse = false;
+    //   }
+    // });
+    // // if (this.allFalse == true){
+    // //   console.log('No bookmarks');
+    // // }
+    // console.log(this.allFalse);
   }
-
+  
+  
 }
