1757bc7a784751433fc6f05f9fb02159f813802d
Kiledjia <56278781+Kiledjia@users.noreply.github.com>
2021-06-02 09:56:57 -0700

    Styling changes

--- src/app/bookmarks/bookmarks.page.ts
+++ src/app/bookmarks/bookmarks.page.ts
@@ -15,9 +15,12 @@ export class BookmarksPage implements OnInit {
   {img: '/assets/imgs/restroom.png', name: 'Restroom ', address: 'Anteater Recreation Center	Room #104', bookmark: true}, 
   {img: '/assets/imgs/langsonLib.png', name: 'Langson Library', address: '23 W Peltason Dr, Irvine CA 92670', bookmark: true},
   {img: '/assets/imgs/PhoenixFoodCourt.png', name: 'Phoenix Food Court', address: 'Ring Rd, Irvine, CA 92697', bookmark: true},
-  {img: '/assets/imgs/bikeParking.png', name: 'Bike Parking', address: 'Engineering Tower, Irvine, CA 92697',  bookmark: true}]
+  {img: '/assets/imgs/bikeParking.png', name: 'Bike Parking', address: 'Engineering Tower, Irvine, CA 92697',  bookmark: true},
+  {img: '/assets/imgs/computerLab.png', name: 'Computer Lab', address: 'ET 201, Plaza Level Engineering Tower',  bookmark: true}]
   bookmarkedcopy = this.bookmarked.slice()
 
+
+  
   constructor(public popoverController: PopoverController,public alertController: AlertController) { 
     
   }
--- src/app/category-page/category-page.page.ts
+++ src/app/category-page/category-page.page.ts
@@ -12,7 +12,7 @@ import { AlertController,ToastController  } from '@ionic/angular';
 })
 export class CategoryPagePage implements OnInit {
 
-  libs:any = [ {"img": '/assets/imgs/langsonLib.png', "name": 'Langson Library', "address": '23 W Peltason Dr, Irvine CA 92670',"dist":320,"bookmark": true}
+  libs:any = [ {"img": '/assets/imgs/langsonLib.png', "name": 'Langson Library', "address": '23 W Peltason Dr, Irvine CA 92670',"dist":320,"bookmark": false}
 ,{"img": '/assets/imgs/gscpic.png', "name": 'Gateway Study Center', "address": '23 W Peltason Dr, Irvine CA 92670',"dist":330, "bookmark": false}
 ,{"img": '/assets/imgs/scilibpic.png', "name": 'Science Library', "address": ' Loading Deck, Irvine CA 92617',"dist":2640, "bookmark": false}
 ,{"img": '/assets/imgs/Lawlib.png', "name": 'Law Library', "address": '401 E Peltason Dr #2000, Irvine CA 92697',"dist":3230,"bookmark": false}];
