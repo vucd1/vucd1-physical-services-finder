e35381e591f810ca17ac819f00802a56a3ce6490
Kiledjia <56278781+Kiledjia@users.noreply.github.com>
2021-06-02 10:18:04 -0700

    More Styling

--- src/app/bookmarks/bookmarks.page.ts
+++ src/app/bookmarks/bookmarks.page.ts
@@ -11,12 +11,14 @@ import { AlertController, } from '@ionic/angular';
   styleUrls: ['./bookmarks.page.scss'],
 })
 export class BookmarksPage implements OnInit {
-  bookmarked:any = [{img: '/assets/imgs/fillingStation.png', name: 'Water Filling Station ', address: 'Rowland Hall, Near First Floor Elevator', bookmark: true}, 
-  {img: '/assets/imgs/restroom.png', name: 'Restroom ', address: 'Anteater Recreation Center	Room #104', bookmark: true}, 
+  bookmarked:any = [
   {img: '/assets/imgs/langsonLib.png', name: 'Langson Library', address: '23 W Peltason Dr, Irvine CA 92670', bookmark: true},
+  {img: '/assets/imgs/fillingStation.png', name: 'Water Filling Station ', address: 'Rowland Hall, Near First Floor Elevator', bookmark: true}, 
+  {img: '/assets/imgs/restroom.png', name: 'Restroom ', address: 'Anteater Recreation Center	Room #104', bookmark: true}, 
   {img: '/assets/imgs/PhoenixFoodCourt.png', name: 'Phoenix Food Court', address: 'Ring Rd, Irvine, CA 92697', bookmark: true},
   {img: '/assets/imgs/bikeParking.png', name: 'Bike Parking', address: 'Engineering Tower, Irvine, CA 92697',  bookmark: true},
-  {img: '/assets/imgs/computerLab.png', name: 'Computer Lab', address: 'ET 201, Plaza Level Engineering Tower',  bookmark: true}]
+  {img: '/assets/imgs/computerLab.png', name: 'Computer Lab', address: 'ET 201, Plaza Level Engineering Tower',  bookmark: true}
+  ]
   bookmarkedcopy = this.bookmarked.slice()
 
 
